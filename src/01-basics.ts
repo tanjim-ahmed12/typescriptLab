31a1ad46dbc4d2df3a682f6eb64283d4f381bf47
healt-tracker-rest <fardin1281@gmail.com>
2025-02-06 21:07:49 +0000

    Refactor - Add type declaration file: disable redundant console logs

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -1,10 +1,12 @@
 // (Optional) Change the names below to your friends.
 
-interface Friend {
-    name: string;
-    phone: string;
-    age: number
-}
+import {Friend, Colleague, ColleagueHistory } from './myTypes'
+
+// interface Friend {
+//     name: string;
+//     phone: string;
+//     age: number
+// }
 
 const friend1 = {
     name: "Paul Fleming",
@@ -18,23 +20,23 @@ const friend1 = {
     age: 31,
   };
   
-  const friends = [friend1, friend2];
-  console.log(friends[1]);
+ export const friends = [friend1, friend2];
+//   console.log(friends[1]);
   
   //   -------------------
 
-  interface Colleague{
-    name: string;
-    department: string;
-    contact: {
-        email: string;
-        extension: number
-    }
-  }
-  interface ColleagueHistory {
-    current: Colleague[],
-    former: Colleague[]
-  }
+//   interface Colleague{
+//     name: string;
+//     department: string;
+//     contact: {
+//         email: string;
+//         extension: number
+//     }
+//   }
+//   interface ColleagueHistory {
+//     current: Colleague[],
+//     former: Colleague[]
+//   }
   
 
   const colleague1 = {
@@ -69,5 +71,5 @@ const friend1 = {
   };
   
   
-  console.log(colleagues.current[0]);
+//   console.log(colleagues.current[0]);
   
